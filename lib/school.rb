d419ffa51433678bea2535660d7292e6b0fabd24
Shayla White <shayladwhite1@gmail.com>
2020-02-05 19:33:47 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,4 +1,3 @@
-require "pry"
 class School
 
       attr_reader :roster
@@ -10,23 +9,19 @@ class School
       def add_student(name, grade)
         # ||= or equals
         @roster[grade] ||= []
-        @roster[grade].push(name)
+        @roster[grade] << name
+        
       end
-
       def grade(grade)
         @roster[grade]
       end
-
-      def sort
-
-         @roster.values.each {|x| x.sort!}
+  def sort
+    
+             @roster.values.each {|x| x.sort!}
          @roster
-         #@roster.values.sort
-        # roster_sorted = @roster
-        # roster_sorted.sort
+        
 
-
-        #binding.pry
-
+  end
       end
-end
+    
+ 
